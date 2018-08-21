f082446c51e67ebd49ec31d205742fe8fd97279c
Maxim <kavbweur@live.ru>
2018-08-21 18:23:34 +0400

    [Fun] fix roll and sword now is a Fate

--- fun/fun.py
+++ fun/fun.py
@@ -8,21 +8,38 @@ from random import choice
 
 
 class Fun:
-    """ПРостые команды."""
+    """Простые команды, возможно весёлые."""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
     async def sword(self, ctx, *, user: discord.Member):
-        """Дуэль на мечах!"""
+        """Битва за Святой Грааль!"""
+        servant_class = "Saber", "Lancer", "Archer", "Rider", "Caster", "Assassin", "Berserker"
         author = ctx.message.author
+        servant = choice(servant_class)
+        enemy_servant = choice(servant_class)
+        while servant == enemy_servant:
+            enemy_servant = choice(servant_class)
+        await self.bot.say("**Вы участвуете в войне за Святой Грааль.**")
+        await self.bot.say("*идёт призыв случайного слуги*")
+        await self.bot.say("Вы призвали слугу класса: **{}**".format(servant))
         if user.id == self.bot.user.id:
-            await self.bot.say("Ты не можешь сражаться со мной.")
-        else:
-            await self.bot.say(author.mention + " и " + user.mention + " сражались " + str(randint(2, 120)) +
-                               " многочисленных часов! Это было долго, потно, но " +
-                               choice([author.mention, user.mention]) + " стал(а) победителем!\nhttps://i.imgur.com/u76vtsP.gifv")
+            await self.bot.say("Вы вызвали Монику на дуэль.\n*Моника меняет реальность*\nВаш слуга погибает, вы не можете продолжать свой участие в войне за святой грааль.\nhttps://i.imgur.com/wlabDWZ.png")
+            return
+        if user.id == author.id:
+            await self.bot.say("Вы приказали вашему слуге атаковать себя командным заклинанием.\n*слуга умирает, проклиная вас*\nВы не можете больше продолжить своё участие в войне за Святой Грааль.\nhttps://i.imgur.com/wlabDWZ.png")
+            return
+        await self.bot.say("Вы встретили врага, класс его слуги: **{}**".format(enemy_servant))
+        await self.bot.say("**НАЧИНАЕТСЯ БИТВА МЕЖДУ СЛУГАМИ**\nhttps://i.imgur.com/u76vtsP.gifv")
+        winner = choice([author.name, user.name])
+        loser = choice([author.name, user.name])
+        while winner == loser:
+            loser = choice([author.name, user.name])
+        await self.bot.say("Побеждает: **{}** со своим слугой. Он(а) стал(а) на шаг ближе к победе.\nА **{}** отправляется в додзё тигры.\nhttps://i.imgur.com/wlabDWZ.png".format(winner, loser))
+        
+
 
     @commands.command(pass_context=True)
     async def love(self, ctx, user: discord.Member):
@@ -30,6 +47,8 @@ class Fun:
         author = ctx.message.author
         if user.id == self.bot.user.id:
             await self.bot.say("Я люблю себя на 100%" )
+        if user.id == author.id:
+            await self.bot.say("Я не могу сказать насколько сильно ты любишь себя.")
         else:
             await self.bot.say("Возможность того, что " + author.mention + " ❤любит❤ " + user.mention + " примерно " +
                                str(randint(0, 100)) + "%!")
@@ -44,6 +63,7 @@ class Fun:
     async def _commands(self):
         """Командовать ботом"""
         await self.bot.say("Не говори мне что делать!")
+
     @commands.command(pass_context=True)
     async def elpsy(self, ctx):
         """Эл псай Ойшидесу!"""
@@ -52,17 +72,29 @@ class Fun:
         await self.bot.say(embed=em)
 
     @commands.command(pass_context=True)
-    async def roll(self, ctx, dice : int = 1, modifier : int = 20):
-        """Бросить кубики. Не вводить более, чем 410 410."""
+    async def roll(self, ctx, dice : str):
+        """Бросить кубики. Пример: 1d20"""
         s = 0
-        numbers = ""
+        twised = dice.split('d')
+        dice = twised[0]
+        try:
+            modifier = twised[1]
+        except IndexError:
+            await self.bot.say("Вы неправильно ввели команду. Пример: 1d20")
+            return
+        numbers = []
         author = ctx.message.author
+        dice = int(dice)
+        modifier = int(modifier)
         dice2 = dice
         for dice in range(dice):
-            n = str(randint(0,modifier))
+            n = int(randint(1,modifier))
             s = int(s) + int(n)
-            numbers = numbers + str(n) + ", "
-        await self.bot.say("{}, *бросаю кубики* :game_die: {}d{} - [{}] = **{}** :game_die:".format(author.name, dice2, modifier, numbers, s))
+            numbers.append(n)
+        try:
+            await self.bot.say("{}, *бросаю кубики* :game_die: {}d{} - {} = **{}** :game_die:".format(author.name, dice2, modifier, numbers, s))
+        except:
+            await self.bot.say("Не удалось отправить сообщение, так как оно было слишком длинным.")
 
 
 def setup(bot):
